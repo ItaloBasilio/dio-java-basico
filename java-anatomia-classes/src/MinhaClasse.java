20e1a3b4b8c9f50dd2f4277a9fc2f15c05afbbc6
Italo Basilio da Silva <italobasilio@gmail.com>
2024-04-26 00:46:05 -0300

    update

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,23 @@
+public class MinhaClasse {
+
+    public static void main (String [] args ){
+        
+        //Neste caso, pode ser alterada
+        int ano = 2021;
+        ano = 2022;
+
+        final String BR = "Brasil";//nao pode ser alterada - constante
+
+    }
+    
+}
+
+
+
+// Classe executavel
+//-> metodo principal ( main )
+
+// System, preciso que voce de uma saida, printando na tela a mensagem
+
+// Variavel
+// toda variavel deve ser iniciada com letra minuscula
